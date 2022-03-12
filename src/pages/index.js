bdca83a6213a7d4359b2b7c7aa5227a9a8b0bfd1
tambarie <gt.gbaragbo@gmail.com>
2022-03-12 22:42:01 +0100

    added

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from './HomePage'
+import Products from '../pages/ProductsPage'
+import About from '../pages/AboutPage'
+import AuthWrapper from '../pages/AuthWrapper'
+import Cart from '../pages/CartPage'
+import Checkout from '../pages/CheckoutPage'
+import Error from '../pages/ErrorPage'
+import Private from '../pages/PrivateRoute'
+import SingleProduct from '../pages/SingleProductPage'
+
+export {
+  Home,
+  Products,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Private,
+  SingleProduct,
+}
